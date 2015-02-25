75ae727e64a3e3d91b12a432dfeb0f7e5cfe9792
Anuj Tomar <ankuto@gmail.com>
2015-02-25 12:09:58 -0500

    updated basecase

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,13 +6,24 @@
 
 var stringifyJSON = function(obj) {
 
-	if(obj === null || typeof obj === Number || typeof obj=== Boolean || typeof obj=== String){
-		return obj.toString();
+	if(obj === null || typeof obj === "number" || typeof obj === "boolean" ){
+		return obj+"";
+	}
+	else if(typeof obj === "string"){
+		return '"'+obj+'"';
 	}
 
-	_.each(obj, function(item){
-		stringifyJSON(item);
-	})
+
+	if(obj instanceof Array){
+		var arr = "[";
+		index = -1;
+
+		if(index === obj.length){
+			arr+= "]";
+			return arr;
+		}
+		return arr+=stringifyJSON(obj[index+1]);
+	}
 
 
 	
