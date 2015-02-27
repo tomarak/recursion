8fa8f022a8eca3077ecbe8d12fac97ea568aa131
Anuj Tomar <ankuto@gmail.com>
2015-02-26 19:06:15 -0500

    completed getElementsbyClassName, used document instead of document.body though

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+
+  var elements = document;
+  console.log(elements);
+  var results = [];
+ 
+ function scan(node){
+ 	for(var i = 0; i < node.childNodes.length; i++){
+ 		if(node.childNodes[i].classList && node.childNodes[i].classList.contains(className)){
+ 			results.push(node.childNodes[i]);
+ 		}
+ 			scan(node.childNodes[i]);
+ 	}
+ }
+ scan(elements);
+ console.log(results);
+ return results;
+
+  
 };
+
+
+//document.body, element.childNodes, and element.classList
